f3ffce00fbc28317775ab7b708e82752fcf194b8
Soha Ahmed Hamdy <soha7hmed@gmail.com>
2023-07-26 18:54:43 +0300

    Initial Commit

---
+++ National Geographic/National_GeographicApp.swift
@@ -0,0 +1,17 @@
+//
+//  National_GeographicApp.swift
+//  National Geographic
+//
+//  Created by Soha Ahmed Hamdy on 26/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct National_GeographicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
